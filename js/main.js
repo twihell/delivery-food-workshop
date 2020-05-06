d1661d872318ef72c631706bb882ec34eea9fb33
twihell <sevlost@gmail.com>
2020-05-06 23:34:08 +0300

    add a slider and comments to the js file

--- js/main.js
+++ js/main.js
@@ -1,36 +1,54 @@
-"use strict";
+"use strict"; //strict mode that checks whether js syntax rules are followed properly;
 
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const closeBtn = document.querySelector(".close");
-const buttonAuth = document.querySelector(".button-auth");
-const modalAuth = document.querySelector(".modal-auth");
-const buttonCloseAuth = document.querySelector(".close-auth");
-const authForm = document.querySelector("#logInForm");
-const loginInput = document.querySelector("#login");
-const userName = document.querySelector(".user-name");
-const buttonOut = document.querySelector(".button-out");
-const errorMessage = document.querySelector(".alert-modal");
-const closeError = document.querySelector(".close-error");
-const cardsRestaurants = document.querySelector(".cards-restaurants");
-const containerPromo = document.querySelector(".container-promo");
-const allRestaurants = document.querySelector(".restaurants");
-const menu = document.querySelector(".menu");
-const logo = document.querySelector(".logo");
-const cardsMenu = document.querySelector(".cards-menu");
+//all variables needed for the code work are created at the very beginning of the js document;
+const cartButton = document.querySelector("#cart-button"),
+modal = document.querySelector(".modal"),
+closeBtn = document.querySelector(".close"),
+buttonAuth = document.querySelector(".button-auth"),
+modalAuth = document.querySelector(".modal-auth"),
+buttonCloseAuth = document.querySelector(".close-auth"),
+authForm = document.querySelector("#logInForm"),
+loginInput = document.querySelector("#login"),
+userName = document.querySelector(".user-name"),
+buttonOut = document.querySelector(".button-out"),
+errorMessage = document.querySelector(".alert-modal"),
+closeError = document.querySelector(".close-error"),
+cardsRestaurants = document.querySelector(".cards-restaurants"),
+containerPromo = document.querySelector(".container-promo"),
+menu = document.querySelector(".menu"),
+allRestaurants = document.querySelector(".restaurants"),
+cardsMenu = document.querySelector(".cards-menu"),
+logo = document.querySelector(".logo");
 
 let loginVar = localStorage.getItem("gloDelivery");
 
+
+//Function declarations/expressions go right after variables;
+
+//This function validates whether login name is correct via regular expression;
+const valid = function (str) {
+  if (str != null) {
+    const nameReg = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/; /*login should be min 2, max 20 symbols; it can contain
+  capital and lowercase letters, numbers, first symbol must be a letter; */
+    return nameReg.test(str);
+  } else {
+    return false;
+  }
+};
+
+//This function checks whether modal window for cart checkout is opened or closed.
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
 
+//This function checks whether modal login window is open. If it's open, error box can be shown and vice versa.
 function toggleModalAuth() {
   modalAuth.classList.toggle("is-open");
   errorMessage.classList.remove("alert-modal-show");
 }
 
-
+/*This finction logs user out from the website if the logout button is clicked; 
+in other cases, userName is shown near the logout button;*/
 function authorizedUser() {
   console.log("Success");
 
@@ -54,24 +72,22 @@ function authorizedUser() {
   buttonOut.addEventListener("click", logOut);
 }
 
-function maskInput(string) {
-  return string != null && !!string.trim();
-}
-
+//this function logs user in after they click the login button;
 function notAuthorizedUser() {
 
   function logIn(event) {
     event.preventDefault();
-    loginVar = loginInput.value;
+    loginVar = loginInput.value.trim();
     localStorage.setItem("gloDelivery", loginVar);
 
-    if (maskInput(loginVar)) {
+    if (valid(loginVar)) {
       toggleModalAuth();
       buttonAuth.removeEventListener("click", toggleModalAuth);
       buttonCloseAuth.removeEventListener("click", toggleModalAuth);
       authForm.removeEventListener("submit", logIn);
     } else {
       errorMessage.classList.add("alert-modal-show");
+      loginInput.value = "";
     }
 
     authForm.reset();
@@ -86,8 +102,9 @@ function notAuthorizedUser() {
 
 }
 
+//this function checks whether the user is logged in or not;
 function checkAuth() {
-  if (maskInput(loginVar)) {
+  if (valid(loginVar)) {
     authorizedUser();
 
   } else {
@@ -98,6 +115,7 @@ function checkAuth() {
 
 }
 
+//this function generates an html card with a restaurant on the main page;
 function createCardRestaurant() {
   const card = `
       <a class="card card-restaurant">
@@ -120,6 +138,7 @@ function createCardRestaurant() {
   cardsRestaurants.insertAdjacentHTML("beforeend", card);
 }
 
+//this function generates dishes for every restaurant card;
 function createCardGood() {
   const card = document.createElement("div");
   card.className = "card";
@@ -146,8 +165,10 @@ function createCardGood() {
   cardsMenu.insertAdjacentElement("beforeend", card);
 }
 
+/*this function opens restaurant dishes after the user clicked on any of them. if they're not logged in, 
+they will be asked to log in first*/
 function openGoods(event) {
- 
+
   if (loginVar) {
     const target = event.target;
     const restaurant = target.closest(".card-restaurant"); //this method looks for the closest element with the given name;
@@ -171,26 +192,32 @@ function openGoods(event) {
 
 }
 
+//event listeners are added after all function declarations/expressions;
 
-
+//this event listener registers every clieck on a cart button;
 cartButton.addEventListener("click", toggleModal);
 
+//this event listener registers every click on the close button of the card modal window;
 closeBtn.addEventListener("click", toggleModal);
 
+//this listener registers every click on any of the restaurant cards;
 cardsRestaurants.addEventListener("click", openGoods);
 
+//this listener registers every click on the logo of the website;
 logo.addEventListener("click", function () {
   containerPromo.classList.remove('hide');
   allRestaurants.classList.remove('hide');
   menu.classList.add('hide');
 
-  
+
 });
 
+//this listener registers every click on the close button placed in the error message box;
 closeError.addEventListener("click", function () {
   errorMessage.classList.remove("alert-modal-show");
 })
 
+//the last elements in the js focument are function callbacks;
 
 
 checkAuth();
@@ -198,3 +225,12 @@ checkAuth();
 createCardRestaurant();
 createCardRestaurant();
 createCardRestaurant();
+
+//This is an inicialization code of the slider provided by the Swiper library;
+new Swiper(".swiper-container", {
+  loop: true,
+  autoplay: {
+    delay: 3000
+  },
+  slidesPerView: 1,
+});
